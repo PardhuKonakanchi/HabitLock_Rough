2fcb491464d3bdca79e4dbf293ab973fd5136211
PardhuKonakanchi <pardhukonakanchi@berkeley.edu>
2019-01-14 16:22:10 -0800

    First Upload

---
+++ app/src/main/java/com/example/pardhu/habitlock_rough/UserEntryActivity.java
@@ -0,0 +1,13 @@
+package com.example.pardhu.habitlock_rough;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_entry);
+    }
+}
