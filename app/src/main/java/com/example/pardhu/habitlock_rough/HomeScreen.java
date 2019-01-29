ff1907a56946215540f9a1063123f5fc4beb79d6
Pardhu Konakanchi <pardhukonakanchi@berkeley.edu>
2019-01-28 17:46:57 -0800

    Added in CardView into home screen to soon add Habit graph fragments

--- app/src/main/java/com/example/pardhu/habitlock_rough/HomeScreen.java
+++ app/src/main/java/com/example/pardhu/habitlock_rough/HomeScreen.java
@@ -2,8 +2,10 @@ package com.example.pardhu.habitlock_rough;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.ListFragment;
 import android.view.View;
 import android.support.design.widget.NavigationView;
