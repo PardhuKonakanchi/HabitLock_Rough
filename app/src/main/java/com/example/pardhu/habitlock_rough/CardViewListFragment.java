24b01319b616aac0fe0308fb055cb620a1f8b8d9
Pardhu Konakanchi <pardhukonakanchi@berkeley.edu>
2019-01-29 11:21:46 -0800

    Added listview design and fragment to import later to homescreen

---
+++ app/src/main/java/com/example/pardhu/habitlock_rough/CardViewListFragment.java
@@ -0,0 +1,17 @@
+package com.example.pardhu.habitlock_rough;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CardViewListFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.graph_listview, container, false);
+    }
+}
